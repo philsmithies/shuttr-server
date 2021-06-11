53a04b00d4d62b3f21f328d1058ed5c80d9e405c
“Phil <philsmithies@gmail.com>
2021-06-11 13:13:47 +0200

    server updated

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // pulls in the express library
 const express = require('express')
 const pool = require('./config/db')
+const { cloudinary } = require('./utils/cloudinary');
 
 // allows us to write app and the crud action we want ex. app.get | app.post | app.delete etc...
 const app = express()
 const cors = require('cors')
+
+// boiler plate on all the images to stop giant images. 
+app.use(express.static('public'));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 // middleware
 app.use(express.json()) // =>  allows us to read the request or req body
@@ -12,10 +18,10 @@ app.use(cors())
 
 //////////// Routes (to be filled out later in tutorial)
 
-
+const port = process.env.PORT || 3001
 // define what localhost port we want our server to run on
-app.listen(3000, ()=> {
-    console.log(`Server running on port: 3000`)
+app.listen(port, ()=> {
+    console.log(`Server running on port: ${port}`)
 })
 
 app.get('/', (req, res) => {
@@ -60,5 +66,31 @@ app.get('/users/:id', async (req, res) => {
       res.json(user.rows[0])
   } catch (err) {
       console.error(err.message)
+  }
+})
+ 
+app.get('/images', async(req, res) => {
+  const {resources} = await cloudinary.search.expression
+  ('folder: cyber_photos').sort_by('public_id', 'desc')
+  .max_results(30)
+  .execute()
+  const publicIds = resources.map( file => file.public_id)
+  // gets a array response of all the public ids we can use to put on the page
+  res.send(publicIds) 
+})
+
+// photos upload
+app.post('/uploadImage', async (req, res)=> {
+  try {
+    const fileStr = req.body.data
+    const uploadedResponse = await cloudinary.uploader.upload(
+      fileStr, {
+      upload_preset: 'cyber_photos'
+    })
+    console.log(uploadedResponse)
+    res.json({msg: "WOOP WOOP"})
+  } catch (error){
+    console.error(error)
+    res.status(500).json({err: 'something is going bad'})
   }
 })
