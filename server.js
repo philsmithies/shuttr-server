97bb81a1e5e43413b6ff2a8a51cdfb8bcd7f38dd
Tiago Manuel <79860571+TiagoManuelPC@users.noreply.github.com>
2021-06-11 10:39:38 +0100

    mango db working

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 // pulls in the express library
-const express = require('express')
-const pool = require('./config/db')
-const morgan = require("morgan")
+const express = require('express');
+const pool = require('./config/db');
+const morgan = require("morgan");
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const User = require("./users");
+
+mongoose.connect("mongodb+srv://admin:adminpassword@cluster0.xu6qx.mongodb.net/cyberPlayground?retryWrites=true&w=majority", 
+{
+userNewParser: true,
+useUnifiedTopology: true
+},
+()=>{
+  console.log("Mongoose is Conected");
+}
+);
 
 // allows us to write app and the crud action we want ex. app.get | app.post | app.delete etc...
 const app = express()
-const cors = require('cors')
+
 
 // middleware
 app.use(express.json()) // =>  allows us to read the request or req body
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
 app.use(morgan('tiny'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: 'secretecode',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+app.use(cookieParser('secretecode'));
+
 
 //////////// Routes (to be filled out later in tutorial)
 
@@ -27,7 +59,23 @@ app.get('/', (req, res) => {
 })
 
 // create a user
-app.post('/users', async (req, res) => {
+app.post('/users',  (req, res) => {
+  User.findOne({username: req.body.username}, async (err, doc)=>{
+    if (err) throw err;
+    if (doc) res.send('User Already Exists');
+    if (!doc){
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send('User Created');
+    }
+  });
+});
+  
+app.post('/login', async (req, res) => {
   try {
       // await
       console.log(req.body)
@@ -35,8 +83,8 @@ app.post('/users', async (req, res) => {
       const { username, password, email} = req.body
 
       const newUser = await pool.query(
-          "INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *", // returning * lets us see the data in the json response
-          [username, password, email]
+          "SELECT * users WHERE username = $1 AND password = $2", // returning * lets us see the data in the json response
+          [username, password]
       ) 
       res.json(newUser.rows[0])
   } catch (err) {
@@ -67,3 +115,20 @@ app.get('/users/:id', async (req, res) => {
       console.error(err.message)
   }
 })
+
+
+// try {
+//   // await
+//   console.log(req.body)
+  
+//   const { username, password, email} = req.body
+
+//   const newUser = await pool.query(
+//       "INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *", // returning * lets us see the data in the json response
+//       [username, password, email]
+//   ) 
+//   res.json(newUser.rows[0])
+// } catch (err) {
+//   console.error(err.message)
+// }
+// })
