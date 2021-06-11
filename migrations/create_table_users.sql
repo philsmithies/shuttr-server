0b50999466da5c9f66fe13f2e9b9e593813eec1c
Phil Smithies <60888215+phileeep@users.noreply.github.com>
2021-06-11 16:02:45 +0200

    Delete create_table_users.sql

--- migrations/create_table_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-  user_id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-  email VARCHAR ( 200 ) UNIQUE NOT NULL,
-  password VARCHAR (20) NOT NULL,
-  username VARCHAR UNIQUE NOT NULL,
-);
