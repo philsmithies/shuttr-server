7abfdccc7a1f5d8a5a42e0fe16d843ed87709c9d
Tiago Manuel <79860571+TiagoManuelPC@users.noreply.github.com>
2021-06-12 16:53:12 +0100

    user can login logout

--- user.js
+++ user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 const user = new mongoose.Schema({
-  name: String,
-  username: String,
-  password: String,
-  email: String,
+  name:{ type: String, required: true},
+  username: {type: String, unique: true,required:true},
+  password: {type: String, required: true},
+  email: {type: String, unique: true,required:true},
   
 });
+user.plugin(uniqueValidator, { message: 'Error, expected {VALUE} to be unique'})
 module.exports = mongoose.model("User", user);
