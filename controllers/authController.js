7bb79e419c57ad53b9b1c8dbdfdba1268977aef1
Phil Smithies <philsmithies@gmail.com>
2021-11-26 13:06:07 +0100

    brcypt updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-nodejs");
 const cookieParser = require("cookie-parser");
 
 // create a user
