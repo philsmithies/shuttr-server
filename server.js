1202fa5c94a8129614ed20922a268863fd237687
Phil Smithies <philsmithies@gmail.com>
2022-03-29 13:30:26 +0200

    jwt

--- server.js
+++ server.js
@@ -32,14 +32,9 @@ mongoose.connect(
 const app = express();
 let server = require("http").Server(app);
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // For legacy browser support
-};
-
 // middleware
 app.use(express.json()); // =>  allows us to read the request or req body
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
