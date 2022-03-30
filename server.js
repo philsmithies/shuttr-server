2b75f42fcfdf8dbf625bfd8697189164ace88551
Phil Smithies <philsmithies@gmail.com>
2022-03-30 08:57:04 +0200

    server links updated

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json()); // =>  allows us to read the request or req body
 app.use(
   cors({
     credentials: true,
-    origin: "https://shuttr-fe.vercel.app",
+    origin: "https://shuttr.vercel.app",
   })
 );
 app.use(morgan("tiny"));
